4088b5df1b67e7499078c0782a1f60e831cc8e15
Stellareign <tuguran@mail.ru>
2022-10-27 21:21:21 +0300

    Задача 1 + 2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашка 6,задача 1:");
        int clientOS = 1;
-
+       int year = 2010;
+       boolean yearOSFull = year > 2015;
+        boolean yearOSLite = year < 2015;
+        if (yearOSFull) {
        switch (clientOS) {
            case 0:
                System.out.println("Установите версию приложения для iOS по ссылке.");
                break;
-           case 1:
-               System.out.println("Установите версию приложения для Android по ссылке.");
-               break;
+               case 1:
+           System.out.println("Установите версию приложения для Android по ссылке.");
+           break;
        }
+       }
+        if (yearOSLite) {
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите облегчённую версию приложения для iOS по ссылке.");
+                    break;
+                case 1:
+                    System.out.println("Установите облегчённую версию приложения для Android по ссылке.");
+                    break;
+            }
+        }
        }
     }
