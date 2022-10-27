89473271542f7c2c77889dc7d28f6427dbca7122
Stellareign <tuguran@mail.ru>
2022-10-27 21:24:01 +0300

    Задача 1 + 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашка 6,задача 1:");
+        System.out.println("Домашка 6,задача 1+2:");
        int clientOS = 1;
        int yearOS = 2010;
        boolean yearOSFull = yearOS > 2015;
