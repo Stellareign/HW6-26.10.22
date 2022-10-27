75a402d0eb051fa4e67b77e1069e10f549e615c5
Stellareign <tuguran@mail.ru>
2022-10-27 21:06:50 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашка 6,задача 1:");
+       int clientOS = 1;
+
+       switch (clientOS) {
+           case 0:
+               System.out.println("Установите версию приложения для iOS по ссылке.");
+               break;
+           case 1:
+               System.out.println("Установите версию приложения для Android по ссылке.");
+               break;
+       }
+       }
+    }
